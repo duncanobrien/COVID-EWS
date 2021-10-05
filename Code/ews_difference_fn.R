aaa1cbb409400d5096be0b26d5c6a47cf6c6db36
duncanobrien <duncan.a.obrien@gmail.com>
2021-10-05 11:41:36 +0100

    Annotation updates

--- Code/ews_difference_fn.R
+++ Code/ews_difference_fn.R
@@ -2,6 +2,8 @@
 
 extract.ews.difference <- function(ews.data, deriv.data,data_source,sensitivity){
   
+  require(tidyverse)
+  
   out.dat <- ews.data %>%
     select(-cases)%>% # drop columns that will be duplicated below
     left_join(deriv.data, by = "true.date")%>% # merge EWS data and model derivative data
@@ -11,7 +13,7 @@ extract.ews.difference <- function(ews.data, deriv.data,data_source,sensitivity)
               first.ews = true.date[rep(rle(threshold.crossed)$lengths >= sensitivity & rle(threshold.crossed)$value == 1,times = rle(threshold.crossed)$lengths)][1])%>%
     #^ extract the date index of the first period of positive first derivative (i.e. when 'deriv.direction' = 1) that persists for longer than a week ('lengths > 7')
     # and extract the date index of the first detection of an EWS (i.e. when 'threshold.crossed' = 1)  that persists for length 'sensitivity' 
-    mutate(day.prior = difftime(first.deriv, first.ews, units = "days"), # calculate difference between date of derivative change and first EWS detection
+    mutate(day.prior = base::difftime(first.deriv, first.ews, units = "days"), # calculate difference between date of derivative change and first EWS detection
            data.source= data_source)  %>%
     ungroup()%>%
     mutate(prediction = ifelse(is.na(day.prior) & is.na(first.deriv) & !is.na(first.ews),"unknown",
