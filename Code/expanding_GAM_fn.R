4c424a85d0cbe03c4294177b11b53dbb0c735384
duncanobrien <duncan.a.obrien@gmail.com>
2021-10-06 14:03:19 +0100

    Updated annotations

--- Code/expanding_GAM_fn.R
+++ Code/expanding_GAM_fn.R
@@ -4,7 +4,7 @@
 
 expanding.gam <- function(mod.dat,sensitivity,train.length){
   #@mod.dat = data frame consisting of number of COVID-19 cases, date (numeric) and day-of-week (integer)
-  #@sensitivity = length of time stationary persists for after a decrease to be acknowledged
+  #@sensitivity = length of time a stationary period persists for after a decrease of the same length for the decrease to be acknowledged
   #@train.length = minimum number of data points allocated to model fitting
   
   library(mgcv)
@@ -12,7 +12,7 @@ expanding.gam <- function(mod.dat,sensitivity,train.length){
   set.seed(123)
   
   #curve.direction fn for identifying direction of change in GAM derivatives from
-  #Burthe SJ et al. 2016 Do early warning indicators consistently predict 
+  #Burthe et al. 2016 Do early warning indicators consistently predict 
   #nonlinear change in long-term ecological data? J. Appl. Ecol. 53, 
   #666–676. (doi:https://doi.org/10.1111/1365-2664.12519)
   curve.direction <- function(x=0, L.CI, U.CI){
@@ -30,7 +30,7 @@ expanding.gam <- function(mod.dat,sensitivity,train.length){
     end.pt <- start + train.length -1 #re-initiate window end point based previous window
     
     while(isFALSE(success) & end.pt < (dim(mod.dat)[1])){ #if a stationary period is not detected and model doesn't exceed length of data, fit GAM up to current end.pt
-      sub.dat <- na.omit(mod.dat[start:end.pt,]) #subset dataframe 
+      sub.dat <- na.omit(mod.dat[start:end.pt,]) #subset dataframe
       
       if(sum(sub.dat$cases) == 0){ #if all cases 0 (i.e. at start of monitoring), classify as fail and add next data point
         success <- FALSE
@@ -38,7 +38,7 @@ expanding.gam <- function(mod.dat,sensitivity,train.length){
       }else{
         tmp.gam <-  suppressWarnings(mgcv::gam(abs(cases)~ s(Weekday,bs = "cs",k=7)+s(Date, bs="tp", k=dim(sub.dat)[1]/4) , data = sub.dat,
                                                family = gaussian(), method = "REML")) 
-        #fit generic GAM to subsetted dataframe
+        #fit generic GAM to subsetted dataframe. Warnings suppressed as can break function loop
         tmp.deriv <- data.frame(gratia::derivatives(tmp.gam, term = "s(Date)", interval = "confidence",n= dim(sub.dat)[1]),
                                 "deriv.direction" = curve.direction(0, gratia::derivatives(tmp.gam, term = "s(Date)", interval = "confidence",n= dim(sub.dat)[1])$lower, gratia::derivatives(tmp.gam, term = "s(Date)", interval = "confidence",n= dim(sub.dat)[1])$upper)/2)
         #estimate derivatives and direction of change
